71c8ae795f099babfce95025317fac0965f79223
Gabriel <gabrielsb1090@gmail.com>
2026-03-16 14:24:20 -0300

    Criando lugares

--- projeto-viagem/src/Components/data/places.js
+++ projeto-viagem/src/Components/data/places.js
@@ -1,32 +1,89 @@
-import img from "../../assets/imgs/main-img.jpg"
+import paris from "../../assets/imgs/city-imgs/paris.jpg"
+import bordeaux from "../../assets/imgs/city-imgs/bordeux.jpg"
+import lyon from "../../assets/imgs/city-imgs/lyon.jpg"
+
+import newyork from "../../assets/imgs/city-imgs/newyork.jpg"
+import losangeles from "../../assets/imgs/city-imgs/losangeles.jpg"
+import miami from "../../assets/imgs/city-imgs/miami.jpg"
+
+import milano from "../../assets/imgs/city-imgs/milano.jpg"
+import roma from "../../assets/imgs/city-imgs/roma.jpg"
+import turim from "../../assets/imgs/city-imgs/turim.jpg"
+
+import barcelona from "../../assets/imgs/city-imgs/barcelona.jpg"
+import madrid from "../../assets/imgs/city-imgs/madrid.jpg"
+import granada from "../../assets/imgs/city-imgs/granada.jpg"
 
 export const places = [
   {
     id: 1,
-    country: "França",
-    city: "Paris",
-    price: 4200,
-    image: img
+    country: "France",
+    city1: {
+      nome: "Paris",
+      img: paris
+    },
+    city2: {
+      nome: "Bordeaux",
+      img: bordeaux
+    },
+    city3: {
+      nome: "Lyon",
+      img: lyon
+    },
+    price: 4200
   },
+  
   {
     id: 2,
-    country: "França",
-    city: "Bordeaux",
-    price: 3500,
-    image: img
+    country: "USA",
+    city1: {
+      nome: "New York",
+      img: newyork
+    },
+    city2: {
+      nome: "Los Angeles",
+      img: losangeles
+    },
+    city3: {
+      nome: "Miami",
+      img: miami
+    },
+    price: 3500
   },
+  
   {
     id: 3,
-    country: "EUA",
-    city: "New York",
-    price: 5200,
-    image: img
+    country: "Italy",
+    city1: {
+      nome: "Milano",
+      img: milano
+    },
+    city2: {
+      nome: "Roma",
+      img: roma
+    },
+    city3: {
+      nome: "Turim",
+      img: turim
+    },
+    price: 5200
   },
+  
   {
     id: 4,
-    country: "EUA",
-    city: "Los Angeles",
-    price: 4800,
-    image: img
+    country: "Spain",
+    city1: {
+      nome: "Barcelona",
+      img: barcelona
+    },
+    city2: {
+      nome: "Madrid",
+      img: madrid
+    },
+    city3: {
+      nome: "Granada",
+      img: granada
+    },
+    price: 4800
   }
 ]
