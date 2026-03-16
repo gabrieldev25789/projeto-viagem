0ce6dc92c597907abd3b457ea3294512238c8a1f
Gabriel <gabrielsb1090@gmail.com>
2026-03-16 09:39:02 -0300

    Estruturando main

---
+++ projeto-viagem/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+import Main from './Components/Main/Main'
+
+function App() {
+
+  return (
+    <>
+    <NavBar />
+    <Main />
+    </>
+  )
+}
+
+export default App
