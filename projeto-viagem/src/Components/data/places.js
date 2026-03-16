71510ebc22f99ce5440043ea7c127bb88fb1456a
Gabriel <gabrielsb1090@gmail.com>
2026-03-16 11:22:21 -0300

    Criando lugares

---
+++ projeto-viagem/src/Components/data/places.js
@@ -0,0 +1,32 @@
+import img from "../../assets/imgs/main-img.jpg"
+
+export const places = [
+  {
+    id: 1,
+    country: "França",
+    city: "Paris",
+    price: 4200,
+    image: img
+  },
+  {
+    id: 2,
+    country: "França",
+    city: "Bordeaux",
+    price: 3500,
+    image: img
+  },
+  {
+    id: 3,
+    country: "EUA",
+    city: "New York",
+    price: 5200,
+    image: img
+  },
+  {
+    id: 4,
+    country: "EUA",
+    city: "Los Angeles",
+    price: 4800,
+    image: img
+  }
+]
