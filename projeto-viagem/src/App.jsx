0990cdb762853c50430832729aaeafac4ec3f653
Gabriel <gabrielsb1090@gmail.com>
2026-03-16 09:53:08 -0300

    Criando novo NavBar

--- projeto-viagem/src/App.jsx
+++ projeto-viagem/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import NavBar from './Components/NavBar/NavBar'
+import NavBar from './Components/Main/NavBar2/NavBar2'
 import Main from './Components/Main/Main'
 
 function App() {
