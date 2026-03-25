21e292b92653f5a3dde0e5c917551faec834a69f
Gabriel <gabrielsb1090@gmail.com>
2026-03-25 20:23:12 -0300

    Criando função para mostrar cidades quando botão é clidado

--- projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
+++ projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
 import "./PlaceCard.css"
 
-function PlaceCard({id, country, city1, city2, city3, price,}) {
+
+
+function PlaceCard({id, country, city1, city2, city3, price}) {
+
+    const [show, setShow] = useState(false)
+
+    function showCities(){
+        setShow(true)
+    }
   return (
     <>
         <div className="card" key={id}>
+
+
             <h2>{country}</h2>
 
-            <div className="cities">
+            <button onClick={showCities}>Clique para ver cidades</button>
+
+            <div className={show ? "cities" : "hide"}>
 
                 <div className="city">
                     <p>{city1.nome}</p>
