4e1d7dc9df617fb4315773fc4622c49211272353
Gabriel <gabrielsb1090@gmail.com>
2026-03-26 08:14:17 -0300

    Refatorando component

--- projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
+++ projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
@@ -3,22 +3,21 @@ import "./PlaceCard.css"
 import CityCard from "../CityCard/CityCard"
 
 
-function PlaceCard({ id, country, city1, city2, city3 }) {
-  const [show, setShow] = useState(false)
-
-  return (
-    <div className="card" key={id}>
-      <h2>{country}</h2>
-      <button onClick={() => setShow(prev => !prev)}>
-        {show ? "Fechar" : "Clique para ver cidades"}
-      </button>
-      <div className={show ? "cities" : "hide"}>
-        <CityCard city={city1} />
-        <CityCard city={city2} />
-        <CityCard city={city3} />
-      </div>
-    </div>
-  )
+function PlaceCard({ id, country, city1, city2, city3, goCity }) 
+{ const [show, setShow] = useState(false) 
+    return ( 
+    <div className="card" key={id}> 
+    <h2>{country}</h2> 
+    <button onClick={() => setShow(prev => !prev)}> 
+    {show ? "Fechar" : "Clique para ver cidades"} 
+    </button> 
+    <div className={show ? "cities" : "hide"}> 
+    <CityCard city={city1} onGoCity={goCity}/> 
+    <CityCard city={city2} onGoCity={goCity}/> 
+    <CityCard city={city3} onGoCity={goCity}/> 
+    </div> 
+    </div> 
+    ) 
 }
 
 export default PlaceCard
