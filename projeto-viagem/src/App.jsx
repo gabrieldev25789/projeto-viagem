0c542359712571cc6d1366ded7a528f9eeca8ea7
Gabriel <gabrielsb1090@gmail.com>
2026-03-26 20:44:50 -0300

    Mostrando infos da cidade escolhida na tela

--- projeto-viagem/src/App.jsx
+++ projeto-viagem/src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css'
+import { useState } from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import Main from './Components/Main/Main'
 import Places from "./pages/Places"
 
 function App() {
+const [cities, setCities] = useState([])
+const [showText, setShowText] = useState("")
+
+  function goCity(city) {
+    setCities(prev => [...prev, city])
+    setShowText(`i wanna go to ${city.nome} this gonna cost me R$ ${city.price}`)
+  }
 
   return (
     <>
       <NavBar />
       <Main />
-      <Places />
+      <div id="places">
+        <h1 className='places-title'>Choose your destiny wisely</h1>
+      <Places goCity={goCity}/>
+      { showText && <p>{showText}</p> }
+      </div>
     </>
   )
 }
