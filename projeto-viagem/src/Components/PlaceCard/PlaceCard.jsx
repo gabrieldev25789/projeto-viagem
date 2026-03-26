a6c272279c13b6bac00fbe397add7faefb3a018f
Gabriel <gabrielsb1090@gmail.com>
2026-03-26 04:09:06 -0300

    Limpando component/refatorando

--- projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
+++ projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
@@ -1,58 +1,24 @@
 import { useState } from "react"
 import "./PlaceCard.css"
+import CityCard from "../CityCard/CityCard"
 
 
-
-function PlaceCard({id, country, city1, city2, city3}) {
-
-    const [show, setShow] = useState(false)
-    const [showDes, setShowDes] = useState(false)
-
-    function showCities() {
-        setShow(prev => !prev)
-    }
-
-   function seeDescription(city) {
-  setShowDes(prev => prev === city ? null : city)
-}
+function PlaceCard({ id, country, city1, city2, city3 }) {
+  const [show, setShow] = useState(false)
 
   return (
-    <>
-        <div className="card" key={id}>
-
-            <h2>{country}</h2>
-
-            <button onClick={showCities}>{show ? "Fechar" : "Clique para ver cidades"}</button>
-
-            <div className={show ? "cities" : "hide"}>
-
-                <div className="city">
-                    <p>{city1.nome}</p>
-                    <img src={city1.img} alt={city1.nome} />
-                    <p className="price">Price: R$ {city1.price}</p>
-                    <button className="see-description" onClick={() => seeDescription("city1")}>{showDes && showDes === "city1" ? "Close" : "see description"}</button>
-                    <p className={showDes === "city1" ? "description" : "hide"}>{city1.description}</p>
-                </div>
-
-                <div className="city">
-                    <p>{city2.nome}</p>
-                    <img src={city2.img} alt={city2.nome} />
-                    <p className="price">Price: R$ {city2.price}</p>
-                    <button className="see-description" onClick={() => seeDescription("city2")}>{showDes && showDes === "city2" ? "Close" : "see description"}</button>
-                    <p className={showDes === "city2" ? "description" : "hide"}>{city2.description}</p>
-                </div>
-
-                <div className="city">
-                    <p>{city3.nome}</p>
-                    <img src={city3.img} alt={city3.nome} />
-                    <p className="price">Price: R$ {city3.price}</p>
-                    <button className="see-description" onClick={()=> seeDescription("city3")}>{showDes && showDes === "city3" ? "Close" : "see description"}</button>    
-                    <p className={showDes === "city3" ? "description" : "hide"}>{city3.description}</p>
-                </div>
-            </div>
-        </div>
-    </>
-    )
+    <div className="card" key={id}>
+      <h2>{country}</h2>
+      <button onClick={() => setShow(prev => !prev)}>
+        {show ? "Fechar" : "Clique para ver cidades"}
+      </button>
+      <div className={show ? "cities" : "hide"}>
+        <CityCard city={city1} />
+        <CityCard city={city2} />
+        <CityCard city={city3} />
+      </div>
+    </div>
+  )
 }
 
 export default PlaceCard
