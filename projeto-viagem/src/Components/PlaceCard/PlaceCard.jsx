e7671b3989d6f45cc499c2d1d42f43b847446ae5
Gabriel <gabrielsb1090@gmail.com>
2026-03-27 01:43:12 -0300

    Adicionando exibição condicional de cidades e integração com ações de seleção

--- projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
+++ projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
@@ -3,19 +3,23 @@ import "./PlaceCard.css"
 import CityCard from "../CityCard/CityCard"
 
 
-function PlaceCard({ id, country, city1, city2, city3, goCity }) 
-{ const [show, setShow] = useState(false) 
+function PlaceCard({ id, country, city1, city2, city3, goCity, onAddCity }){
+    console.log("PlaceCard onAddCity:", onAddCity) 
+    const [show, setShow] = useState(false) 
     return ( 
     <div className="card" key={id}> 
-    <h2>{country}</h2> 
-    <button onClick={() => setShow(prev => !prev)}> 
-    {show ? "Fechar" : "Clique para ver cidades"} 
-    </button> 
-    <div className={show ? "cities" : "hide"}> 
-    <CityCard city={city1} onGoCity={goCity}/> 
-    <CityCard city={city2} onGoCity={goCity}/> 
-    <CityCard city={city3} onGoCity={goCity}/> 
-    </div> 
+        <h2>{country}</h2> 
+
+        <button onClick={() => setShow(prev => !prev)}> 
+            {show ? "Fechar" : "Clique para ver cidades"} 
+        </button> 
+
+        <div className={show ? "cities" : "hide"}> 
+            <CityCard city={city1} onGoCity={goCity} onAddCity={onAddCity}/> 
+            <CityCard city={city2} onGoCity={goCity} onAddCity={onAddCity}/> 
+            <CityCard city={city3} onGoCity={goCity} onAddCity={onAddCity}/> 
+        </div> 
+
     </div> 
     ) 
 }
