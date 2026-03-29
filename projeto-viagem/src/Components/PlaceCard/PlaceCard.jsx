930b595e064c9184c76e9633c2e05048b59d6c7b
Gabriel <gabrielsb1090@gmail.com>
2026-03-29 11:16:10 -0300

    Removendo função goCity

--- projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
+++ projeto-viagem/src/Components/PlaceCard/PlaceCard.jsx
@@ -3,9 +3,10 @@ import "./PlaceCard.css"
 import CityCard from "../CityCard/CityCard"
 
 
-function PlaceCard({ id, country, city1, city2, city3, goCity, onAddCity }){
-    console.log("PlaceCard onAddCity:", onAddCity) 
+function PlaceCard({ id, country, city1, city2, city3, onAddCity }){
+
     const [show, setShow] = useState(false) 
+    
     return ( 
     <div className="card" key={id}> 
         <h2>{country}</h2> 
@@ -15,11 +16,10 @@ function PlaceCard({ id, country, city1, city2, city3, goCity, onAddCity }){
         </button> 
 
         <div className={show ? "cities" : "hide"}> 
-            <CityCard city={city1} onGoCity={goCity} onAddCity={onAddCity}/> 
-            <CityCard city={city2} onGoCity={goCity} onAddCity={onAddCity}/> 
-            <CityCard city={city3} onGoCity={goCity} onAddCity={onAddCity}/> 
+            <CityCard city={city1} onAddCity={onAddCity}/> 
+            <CityCard city={city2} onAddCity={onAddCity}/> 
+            <CityCard city={city3} onAddCity={onAddCity}/> 
         </div> 
-
     </div> 
     ) 
 }
