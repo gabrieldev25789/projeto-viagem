c18bf93f746d8629de408de5391b51014d956ef8
Gabriel <gabrielsb1090@gmail.com>
2026-04-10 20:44:13 -0300

    Adicionando função chooseCity e renderizando dinamicamente a lista de cidades

--- projeto-viagem/src/App.jsx
+++ projeto-viagem/src/App.jsx
@@ -1,17 +1,42 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import Main from './Components/Main/Main'
-import Places from "./pages/Places"
-import { places } from './Components/data/places'
+import Places from './Components/Places/Places'
 
 function App() {
+    const [show, setShow] = useState(false)
+    const [lista, setLista] = useState([])
 
+  function showDestinations(){
+      setShow((prev)=> !prev)
+  }
 
+  function chooseCity(name, price){
+      console.log(name, price)
+      setLista((prev)=> [...prev, {name, price}])
+  }
+
+    useEffect(()=>{
+      console.log(lista)
+    }, [lista])
 
   return (
     <>
-      <h1>Hello</h1>
+      <NavBar />
+      <Main />
+      <button onClick={() => showDestinations()}>{show ? "Click to close" : "Click to see destinations"}</button>
+      {show && <Places  chooseCity={chooseCity}/>}
+      
+      {lista.map((item, index)=>{
+      console.log(item)
+        return (
+        <span key={index}>
+          <h4>{item.name}</h4>
+          <p>{item.price}</p>
+        </span>
+        )
+      })}
     </>
   )
 }
