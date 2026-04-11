c8b35cc82116c7e4e45176f2ffef93e123905d7c
Gabriel <gabrielsb1090@gmail.com>
2026-04-11 02:38:40 -0300

    Estruturando melhor a exibição do carrinho

--- projeto-viagem/src/Components/Cart/Cart.jsx
+++ projeto-viagem/src/Components/Cart/Cart.jsx
@@ -1,10 +1,46 @@
 import React, { useState } from 'react'
+import "./Cart.css"
 
+function Cart({ lista = [] }) {
+  const [show, setShow] = useState(false)
+
+  const total = lista.reduce((acc, item) => acc + item.price, 0)
+
+  return (
+    <>
+      <button className='cart-btn' onClick={() => setShow((prev) => !prev)}>
+        {show ? "Close cart" : "View cart"}
+      </button>
+
+      {show && (
+        <div className='cart-container'>
+          <div className='cart-header'>
+            <p className='cart-header-label'>Cart</p>
+          </div>
+
+          <div className='cart-body'>
+            {lista.map((item) => (
+              <div className='cart-item' key={item.name}>
+                <h4 className='cart-name'>{item.name}</h4>
+                <p className='cart-price'>USD {(item.price).toFixed(2)}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className='cart-footer'>
+            <p className='cart-total-label'>Total</p>
+            <p className='cart-total-value'>USD {total}</p>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+{/*
 function Cart({ lista }) {
 
     const [show, setShow] = useState(false)
-
- 
 
     function showCart(){
        setShow((prev)=> !prev)
@@ -13,14 +49,15 @@ function Cart({ lista }) {
 
   return (
     <div>
-    <button onClick={() => showCart()}>{show ? "Click to close" : "Click to see cart"}</button> 
+    <button className='cart-btn' onClick={() => showCart()}>{show ? "Click to close" : "Click to see cart"}</button> 
       {show && lista.map((item) => (
-        <div key={item.name}>
-          <h4>{item.name}</h4>
-          <p>{item.price}</p>
+        <div className='cart-container' key={item.name}>
+          <h4 className='cart-name'>City: {item.name}</h4>
+          <p className='cart-price'>Price: USD {(item.price).toFixed(2)}</p>
         </div>
       ))}
     </div>
   )
 }
+*/}
 export default Cart 
