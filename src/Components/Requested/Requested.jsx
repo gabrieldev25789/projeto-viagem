fe68ebeaa4322fde131ca0b58b64f093c3049b24
Gabriel <gabrielsb1090@gmail.com>
2026-04-14 04:20:09 -0300

    Exibindo resumo dos pedidos

--- src/Components/Requested/Requested.jsx
+++ src/Components/Requested/Requested.jsx
@@ -5,17 +5,38 @@ function Requested() {
   const { state } = useLocation()
   const { list = [], total = 0 } = state || {}
 
+    const formatPrice = (value) => {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(value)
+    }
+
+    const formatDate = (date) =>
+      new Date(date).toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric"
+      })
+
   return (
     <div>
       <h2>Order confirmed!!</h2>
       <ul>
         {list.map(item => (
+      <div>
+
+        <img style={{width: "200px"}} src={item.img} alt="city image" />
           <li key={item.id}>
-            {item.name} x {item.amount} — USD {(item.price * item.amount).toFixed(2)}
+            {item.name} x {item.amount} — USD {formatPrice(item.price * item.amount)}
           </li>
+          <li>Departure: {formatDate(item.startDate)}</li>
+          <li>Return: {formatDate(item.endDate)}</li>
+          <li>{item.nights} nights</li>
+        </div>
         ))}
       </ul>
-      <h3>Total: USD {total.toFixed(2)}</h3>
+      <h3>Total: USD {formatPrice(total.toFixed(2))}</h3>
     </div>
   )
 }
