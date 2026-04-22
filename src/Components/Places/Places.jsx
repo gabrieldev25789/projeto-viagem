dd7bf561b19a931ecefc76d8858698dd7a5152b3
Gabriel <gabrielsb1090@gmail.com>
2026-04-22 07:53:10 -0300

    Mostrando apenas o card da cidade quando o user pesquisa por cidade

--- src/Components/Places/Places.jsx
+++ src/Components/Places/Places.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
 import { places } from '../data/places'
 import "./Places.css"
 
-function Places({ placesData = places, chooseCity, selectCity, setSelectCity }) {
+function Places({ placesData = places, chooseCity, selectCity, setSelectCity, citySearch }) {
 
   return (
-    <div className='infos-container'>
+   <div className='infos-container'>
       {placesData.map((place) => (
         <ul className='continent-country-ul' key={place.id}>
           <li className='continent-country-li'>{place.continent}</li>
@@ -13,31 +12,33 @@ function Places({ placesData = places, chooseCity, selectCity, setSelectCity }) 
 
           <li className='cities-li'>
             <ul className='cities-ul'>
-              {place.cities.map((city, index) => (
-                <div
-                  className='city-card-wrapper'
-                  key={city.id}
-                  style={{ animationDelay: `${index * 60}ms`}}
-                >
-                  <li
-                  onClick={() => {chooseCity(city.id, city.name, city.price, city.img), setSelectCity(city.id)}}
-                  className={`city-card ${selectCity === city.id ? "select" : ""}`}
-                  >
-                    <img className='city-img' src={city.img} alt={city.name} />
-                    <div className='city-info'>
-                      <h3 className='city-name'>{city.name}</h3>
-                      <p className='city-price'>USD {(city.price).toFixed(2)}</p>
-                      <p className='city-description'>{city.description}</p>
-                    </div>
-                  </li>
-                </div>
-              ))}
+            
+              {place.cities.filter(city =>
+                  citySearch ? city.name.toLowerCase().includes(citySearch.toLowerCase()) : true
+                )
+                .map((city, index) => (
+                  <div className='city-card-wrapper' key={city.id} style={{ animationDelay: `${index * 60}ms`}}>
+                    <li
+                      onClick={() => { chooseCity(city.id, city.name, city.price, city.img); setSelectCity(city.id) }}
+                      className={`city-card ${selectCity === city.id ? "select" : ""}`}
+                    >
+                      <img className='city-img' src={city.img} alt={city.name} />
+                      <div className='city-info'>
+                        <h3 className='city-name'>{city.name}</h3>
+                        <p className='city-price'>USD {(city.price).toFixed(2)}</p>
+                        <p className='city-description'>{city.description}</p>
+                      </div>
+                    </li>
+                  </div>
+                ))
+              }
             </ul>
           </li>
         </ul>
       ))}
-    </div>
+    </div>  
   )
 }
+
 
 export default Places
