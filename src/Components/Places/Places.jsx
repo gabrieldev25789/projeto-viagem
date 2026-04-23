07f160905551bdbdd2bcd3030298e95f19fe11b6
Gabriel <gabrielsb1090@gmail.com>
2026-04-23 05:38:29 -0300

    Renderizando cidades com base no valor

--- src/Components/Places/Places.jsx
+++ src/Components/Places/Places.jsx
@@ -1,23 +1,25 @@
-import { places } from '../data/places'
 import "./Places.css"
 
-function Places({ placesData = places, chooseCity, selectCity, setSelectCity, citySearch }) {
+function Places({ placesData, chooseCity, selectCity, setSelectCity, citySearch, sortedCities }) {
 
+if (sortedCities) {
   return (
-   <div className='infos-container'>
-      {placesData.map((place) => (
-        <ul className='continent-country-ul' key={place.id}>
-          <li className='continent-country-li'>{place.continent}</li>
-          <li className='continent-country-li'>{place.country}</li>
+    <div className='infos-container'>
+      <div style={{ display: "flex", flexDirection: "column", gap: "1.5rem", width: "100%" }}>
+      {sortedCities
+        .filter(city =>
+          citySearch ? city.name.toLowerCase().includes(citySearch.toLowerCase()) : true
+        )
+        .map((city, index) => {
+          const place = placesData.find(p => p.cities.some(c => c.id === city.id))
 
-          <li className='cities-li'>
-            <ul className='cities-ul'>
-            
-              {place.cities.filter(city =>
-                  citySearch ? city.name.toLowerCase().includes(citySearch.toLowerCase()) : true
-                )
-                .map((city, index) => (
-                  <div className='city-card-wrapper' key={city.id} style={{ animationDelay: `${index * 60}ms`}}>
+          return (
+            <ul className='continent-country-ul' key={city.id}>
+              <div className='city-card-wrapper' style={{ animationDelay: `${index * 60}ms` }}>
+                <li className='continent-country-li'>{place?.continent}</li>
+                <li className='continent-country-li'>{place?.country}</li>
+                <li className='cities-li'>
+                  <ul className='cities-ul'>
                     <li
                       onClick={() => { chooseCity(city.id, city.name, city.price, city.img); setSelectCity(city.id) }}
                       className={`city-card ${selectCity === city.id ? "select" : ""}`}
@@ -25,7 +27,44 @@ function Places({ placesData = places, chooseCity, selectCity, setSelectCity, ci
                       <img className='city-img' src={city.img} alt={city.name} />
                       <div className='city-info'>
                         <h3 className='city-name'>{city.name}</h3>
-                        <p className='city-price'>USD {(city.price).toFixed(2)}</p>
+                        <p className='city-price'>USD {city.price.toFixed(2)}</p>
+                        <p className='city-description'>{city.description}</p>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </div>
+            </ul>
+          )
+        })
+      }
+    </div>
+  </div>
+  )
+}
+
+  return (
+    <div className='infos-container'>
+      {placesData.map((place) => (
+        <ul className='continent-country-ul' key={place.id}>
+          <li className='continent-country-li'>{place.continent}</li>
+          <li className='continent-country-li'>{place.country}</li>
+          <li className='cities-li'>
+            <ul className='cities-ul'>
+              {place.cities
+                .filter(city =>
+                  citySearch ? city.name.toLowerCase().includes(citySearch.toLowerCase()) : true
+                )
+                .map((city, index) => (
+                  <div className='city-card-wrapper' key={city.id} style={{ animationDelay: `${index * 60}ms` }}>
+                    <li
+                      onClick={() => { chooseCity(city.id, city.name, city.price, city.img); setSelectCity(city.id) }}
+                      className={`city-card ${selectCity === city.id ? "select" : ""}`}
+                    >
+                      <img className='city-img' src={city.img} alt={city.name} />
+                      <div className='city-info'>
+                        <h3 className='city-name'>{city.name}</h3>
+                        <p className='city-price'>USD {city.price.toFixed(2)}</p>
                         <p className='city-description'>{city.description}</p>
                       </div>
                     </li>
@@ -36,9 +75,10 @@ function Places({ placesData = places, chooseCity, selectCity, setSelectCity, ci
           </li>
         </ul>
       ))}
-    </div>  
+    </div>
   )
 }
+
 
 
 export default Places
