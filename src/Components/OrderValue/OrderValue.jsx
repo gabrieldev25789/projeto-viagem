ea6902015ba2a10f5d145b497ea7232e8abd6e68
Gabriel <gabrielsb1090@gmail.com>
2026-04-28 06:37:04 -0300

    Manipulando estado hide

--- src/Components/OrderValue/OrderValue.jsx
+++ src/Components/OrderValue/OrderValue.jsx
@@ -1,28 +1,30 @@
 import "./OrderValue.css"
 
-function OrderValue({ handleSort, sortValue, setSortValue, reset, setRemoveClass }) {
+function OrderValue({ handleSort, sortValue, setSortValue, reset, setRemoveClass, hide }) {
 
   function handleChange(value) {
-  setRemoveClass(true)
-  setSortValue(value)
+    setRemoveClass(true)
+    setSortValue(value)
 
-  if (value === "") {
-    reset() 
-  } else {
-    handleSort(value)
+    if (value === "") {
+      reset()
+    } else {
+      handleSort(value)
+    }
   }
-}
 
-return (
-  <div className="sort-wrapper">
-    <label htmlFor="sort">Sort city by price</label>
-    <select id="sort" value={sortValue} onChange={(e) => handleChange(e.target.value)}>
-      <option value="">--</option>
-      <option value="asc">Low to High</option>
-      <option value="desc">High to Low</option>
-    </select>
-  </div>
-)
+  return (
+    !hide && (
+      <div className="sort-wrapper">
+        <label htmlFor="sort">Sort city by price</label>
+        <select id="sort" value={sortValue} onChange={(e) => handleChange(e.target.value)}>
+          <option value="">--</option>
+          <option value="asc">Low to High</option>
+          <option value="desc">High to Low</option>
+        </select>
+      </div>
+    )
+  )
 }
 
 export default OrderValue
